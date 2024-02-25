3b1990805f7deb4862c9975aee8421353eacb822
Steele Shreve <steele@steelesh.dev>
2024-02-25 16:23:38 -0500

    Add unversioned files

---
+++ SingletonPattern/SingletonPattern/Program.cs
@@ -0,0 +1,12 @@
+﻿using SingletonPattern;
+
+var s = Singleton.GetInstance();
+
+s.SetValue("test");
+
+// returns 'test'
+Console.WriteLine(s.GetValue());
+
+// same instance, so still returns 'test'
+var t = Singleton.GetInstance();
+Console.WriteLine(t.GetValue());
